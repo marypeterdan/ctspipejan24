99e54f08cc62639769a2836abb3b3004b52e3010
marypeterdan <marypeterdan@gmail.com>
2024-01-03 10:27:34 +0530

    data

---
+++ src/main/java/com/stackroute/samplepipe/controller/FirstController.java
@@ -0,0 +1,22 @@
+package com.stackroute.samplepipe.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+	
+	@GetMapping("/api/home")
+	public ResponseEntity<?> getHome()
+	{
+		return new ResponseEntity<String>("Welcome",HttpStatus.OK);
+	}
+	
+	@GetMapping("/api/view")
+	public ResponseEntity<?> getall()
+	{
+		return new ResponseEntity<String>("we learnt aws cicd",HttpStatus.OK);
+	}
+}
